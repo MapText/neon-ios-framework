08a5660366fa437af75b7ec533f48d609bcd4ccd
wroluk <wroluk@gmail.com>
2016-11-03 16:53:43 +0100

    Initial commit

---
+++ Neon/Neon/Neon.h
@@ -0,0 +1,19 @@
+//
+//  Neon.h
+//  Neon
+//
+//  Created by Lukasz Wroczynski on 02.11.2016.
+//  Copyright © 2016 MapText. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Neon.
+FOUNDATION_EXPORT double NeonVersionNumber;
+
+//! Project version string for Neon.
+FOUNDATION_EXPORT const unsigned char NeonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Neon/PublicHeader.h>
+
+
